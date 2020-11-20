26c569544edc1dcd1828243dc9932110e10a075b
Student <student@computer.com>
2020-11-21 03:23:09 +0530

    Correction

--- search.php
+++ search.php
@@ -11,13 +11,13 @@
     <script src="fontawesome/js/all.js"></script>
 </head>
 
+<?php 
+	session_start();
+?>
+
 <script>
 	$(function(){
-		$("#search").submit(function(e){
-			e.preventDefault();
-			$('#search-tag').hide();
-			$('#search-result').show(function(){window.scrollTo(0,400)});
-		});
+		window.scrollTo(0,500);
 	});
 </script>
 
@@ -55,7 +55,7 @@
 		<div class="row">
 			<div class="col-md-6">
 			<h1><strong>Local Learning</strong></h1>
-			<form id='search' name="search" action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+			<form  id='search' name="search" action="search_user.php" method="get">
 				<input type="text" name="location" autocomplete="off" placeholder="Enter Area" class="col-md-5 col-sm-12" required />
 				<br/><br/>
 				<input type="text" name="course" autocomplete="off" placeholder="Enter Course" class="col-md-5 col-sm-12" required />
@@ -69,49 +69,60 @@
 		</div>
 		</div>
 	</div>
-	<div id="search-tag">
-		<center><span class="fas fa-search"></span>  SEARCH RESULT WILL APPEAR HERE</center>
-	</div>
-	<div id="search-result" style="display:none;">
-		<div class="map">
-		<?php 
-			for ($i=0; $i < 2; $i++) { 
-				echo '
-			
-			<i class="fas fa-map-marker-alt" alt="mark" data-toggle="modal" data-target="#Modal"
-				style="color: #c73cc7; font-size: 30px; position: relative; top:',rand(10,390),'px; left:',rand(10,390),'; cursor: pointer;" >
-				
-			</i>
-			<div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-			  <div class="modal-dialog" role="document">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <h5 class="modal-title" id="ModalLabel">Name</h5>
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-			          <span aria-hidden="true">&times;</span>
-			        </button>
-			      </div>
-			      <div class="modal-body">
-			        ...
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			        <a href="';
-			        	if(isset($_SESSION)){
-			        		echo "#";
-			        	}
-			        	else
-			        		echo "signup.php";
-			         echo '">
-			        	<button type="button" class="btn btn-primary">View Profile</button>
-			        </a>
-			      </div>
-			    </div>
-			  </div>
-			</div>	
-		';}?>
-		</div>
-	</div>
+	<?php 
+	if (!isset($_SESSION['search_id'])) 
+		echo'
+		<div id="search-tag">
+			<center><span class="fas fa-search"></span>  SEARCH RESULT WILL APPEAR HERE</center>
+		</div>';
+	else{
+		echo '
+		<div id="search-result">
+			<div class="map">';
+				for ($i=0; $i <=2; $i++) {
+					$x=rand(0,90);
+					$r=(int)sqrt(2025-pow($x-45,2));
+					$y=rand(-1*$r,$r)+42;
+					echo '
+					<i class="fas fa-map-marker-alt" alt="mark" data-toggle="modal" data-target="#Modal',$i,'"
+					style="color: #c73cc7; font-size: 30px; position: absolute; top:',$y,'%; left:',$x,'%; cursor: pointer;" >
+					
+				</i>
+				<div class="modal fade" id="Modal',$i,'" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title" id="ModalLabel">Name',$_SESSION['search_id'][$i],'</h5>
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          <span aria-hidden="true">&times;</span>
+				        </button>
+				      </div>
+				      <div class="modal-body">
+				        ...
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				        <a href="';
+				        	if(isset($_SESSION['user_id'])){
+				        		echo "#";
+				        	}
+				        	else
+				        		echo "signup.php";
+				         echo '">
+				        	<button type="button" class="btn btn-primary">View Profile</button>
+				        </a>
+				      </div>
+				    </div>
+				  </div>
+				</div>	
+			';}
+			echo '
+			</div>
+		</div>';
+	}
+	unset($_SESSION['search_id']);
+	?>
+	
 <footer class="bg-dark py-2 text-light">
 	<small><center>© All Rights Reserved</center></small>
 </footer>
---
+++ search_user.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+	$_SESSION['search_id'] = array(1,2,3);
+	header('Location:search.php');
+?>
